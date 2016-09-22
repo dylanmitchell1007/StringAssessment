ae9821f7fb835ef9f0f28904c7cea2f7be47bbe5
dylanmitchell1007 <armadummy@yahoo.com>
2016-09-22 11:43:46 -0500

    New String Assesment with Changes.
    Includes a few changes by myself and some comments from Ben

--- StringAssessment/Main.cpp
+++ StringAssessment/Main.cpp
@@ -9,26 +9,29 @@ int main()
  	printf("Enter Two Different words or T, X.\n \n");
  	char T[255];
  	char x[255];
- 	std::cin.getline(T, 255);
- 	std::cin.getline(x, 255);
- 	String firstString = String(T);
+	cin >> T;
+	cin >> x;
+
+ 	
+	String firstString = String(T);
  	String secondString = String(x);
 	
-	firstString.constCStyle();
-	firstString.findsubstring();
-	std::cout << firstString.findSubStringIndex(2, "Terminate");
-	std::cout << "Print at Index: " << firstString.index(2) << "\n \n";
+	
+	
+	firstString.index(0, 0);
+
 	firstString.compare(secondString);
 	firstString.append("<-Back");
 	firstString.prepend("Front->");
-	int TankT = firstString.Length();
-	int TankX = secondString.Length();
-	std::cout << "Length of first Entry: " << TankT << "\n \n";
-	std::cout << "Length of second Entry: " << TankX << "\n \n";
-
-
+	int y = firstString.Length();
+	int o = secondString.Length();
+	std::cout << "Length of first Entry: " << y << "\n \n";
+	std::cout << "Length of second Entry: " << o  << "\n \n";
+	firstString.constCStyle();
+	std::cout << firstString.findSubStringIndex(2, "Terminate");
 	firstString.Up();
 	firstString.Low();
+	firstString.findsubstring();
 	system("pause");
 	return 0;
 }
